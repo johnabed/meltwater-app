6902f25b42f8a99269218d82946a42908e258327
John Abed <john.abed@hotmail.com>
2019-05-09 00:41:13 -0400

    Censoring logic working and now just implementing front-end basic design

--- client/src/app/documents/documents.component.ts
+++ client/src/app/documents/documents.component.ts
@@ -12,16 +12,20 @@ export class DocumentsComponent implements OnInit {
   submitted = false;
   success = false;
 
-  censors = [];
+  censorList = [];
+  originalDocument = '';
+  modifiedDocument = '';
+  caseSensitive = true;
 
   constructor(private formBuilder: FormBuilder) {
     this.messageForm = this.formBuilder.group({
       censored: ['', Validators.required],
-      document: ['', Validators.required]
+      document: ['', Validators.required],
+      caseSensitive: [true]
     });
   }
 
-  onSubmit(censored: string, document: string) {
+  onSubmit(censored: string, document: string, caseSensitive: boolean) {
     this.submitted = true;
 
     if (this.messageForm.invalid) {
@@ -30,23 +34,34 @@ export class DocumentsComponent implements OnInit {
 
     // Create censor list to parse document text
     this.createCensors(censored);
-    console.log(this.censors);
 
     // Parse document text
+    this.originalDocument = document;
+    this.modifiedDocument = document;
+    this.caseSensitive = caseSensitive;
 
+    for (let i = 0; i < this.censorList.length; i++) {
+      // OPTIONAL: Creates censor mask to replace in document text ("Hello World" => "XXXXX XXXXX")
+      // const replacementString = this.censorList[i].replace(/\S/g, 'X');
+
+      // If capitalization is not important we turn on i flag to ignore case
+      const flags = this.caseSensitive ? 'g' : 'gi';
+      // Censor every occurrence of String in text with 'XXXX'
+      this.modifiedDocument = this.modifiedDocument.replace(new RegExp('\\b' + this.censorList[i] + '\\b', flags), 'XXXX');
+    }
 
     this.success = true;
 
   }
 
   createCensors(str: string) {
-    // Extracts words and phrases uses Regular Expressions
-    this.censors = str.match(/(("|')[^("|')]+("|')|[^("|')\s]+)/g);
+    // Extract words and phrases using Regular Expressions
+    this.censorList = str.match(/(["'][^("|')]+["']|[^,"'\s]+)/g);
 
-    // Removes two sets of quotations surrounding one entry in the censors
-    this.censors.forEach(function(value, index) {
-      this[index] = value.replace(/^"([^"]+)"$/, '$1'); // todo: Implement for single quote
-    }, this.censors);
+    // Remove single and double quotations from being stored in censorList arr
+    this.censorList.forEach(function(value, index) {
+      this[index] = value.replace(/^["']([^"']+)["']$/, '$1');
+    }, this.censorList);
   }
 
   ngOnInit() {
